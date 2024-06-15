0ee814410cfe8c03633921f7411302f5f6c1596b
kavya_r <kavyar.10b@gmail.com>
2024-06-15 23:44:06 +0530

    Implemented "add new todo"(web)

--- web.py
+++ web.py
@@ -1,9 +1,18 @@
 import streamlit as st
 import functions as fun
+todos=fun.call()
+def add_todo():
+    t=st.session_state["newtodo"]+ "\n"
+    todos.append(t)
+    fun.change(todos)
 st.title("My Todo App")
 st.subheader("Hello, let's start!")
 st.write("Being productive one step at a time")
-todos=fun.call()
 for i in todos:
     st.checkbox(i)
-st.text_input(label="",placeholder="Add a todo...")
+st.text_input(label=" ",placeholder="Add a todo...",
+              on_change=add_todo,key="newtodo")
+
+print("hello")
+
+st.session_state
