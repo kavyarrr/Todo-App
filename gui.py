2439f7acbee1544c03ebc2283fb32fc84013f8fa
kavya_r <kavyar.10b@gmail.com>
2024-06-16 00:12:35 +0530

    Implemented "add complete todo"(web)

--- gui.py
+++ gui.py
@@ -1,11 +1,16 @@
 import functions as fun
 import FreeSimpleGUI as fsg
 import time as t
+import os
 
-fsg.theme("black")
+if not os.path.exists("todos.txt"):
+    with open("todos.txt",'w') as f:
+        pass
+
+fsg.theme("DarkPurple4")
 timelabel=fsg.Text('',key="clock")
 label=fsg.Text("Type in a to-do")
-add_button = fsg.Button(size=10,image_source="add.png",tooltip="Add todo",key="Add")
+add_button = fsg.Button("Add")
 input_box = fsg.InputText(tooltip="Enter todo",key="Todo")
 
 edit_button= fsg.Button("Edit")
@@ -13,7 +18,7 @@ list= fsg.Listbox(values=fun.call(),key="todos",
                   enable_events=True,size=[45,10])
 
 exit_button=fsg.Button("Exit")
-comp_button=fsg.Button(size=10,image_source="complete.png",tooltip="Complete todo",key="Complete")
+comp_button=fsg.Button("Complete")
 window = fsg.Window('My To-Do App',
                     layout=[[timelabel],[label],[input_box,add_button],
                             [list, edit_button,comp_button],
@@ -54,8 +59,8 @@ while True:
                 fsg.popup("Please select item first", font=("Helvetica", 20))
         case 'todos':
             window['Todo'].update(value=val['todos'][0])
-        case "Exit":
-            break
         case fsg.WIN_CLOSED:
+            break
+        case "Exit":
             break
 window.close()
--- web.py
+++ web.py
@@ -5,11 +5,17 @@ def add_todo():
     t=st.session_state["newtodo"]+ "\n"
     todos.append(t)
     fun.change(todos)
+
 st.title("My Todo App")
 st.subheader("Hello, let's start!")
 st.write("Being productive one step at a time")
 for i in todos:
-    st.checkbox(i)
+    checkb=st.checkbox(i,key=i)
+    if checkb:
+        todos.remove(i)
+        fun.change(todos)
+        del st.session_state[i]
+        st.experimental_rerun()
 st.text_input(label=" ",placeholder="Add a todo...",
               on_change=add_todo,key="newtodo")
 
